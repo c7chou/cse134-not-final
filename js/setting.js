6235df38f3135a234b67e8ccbff204fd52309959
c7chou <c7chou@ucsd.edu>
2015-11-10 14:38:51 -0800

    button works and sync altogether

--- js/setting.js
+++ js/setting.js
@@ -1,28 +1,101 @@
 
 /* All session turnoff notication*/
 function toggleTurnOff(source) {
-  var checkboxes = document.getElementsByName('turnoff');
-  for(var i=0, n=checkboxes.length;i<n;i++) {
+  var check = document.getElementsByName('turnoff');
+  for(var i=0, n=check.length;i<n;i++) {
     /*alert(JSON.stringify(checkboxes));*/
-    checkboxes[i].checked = source.checked;
+    check[i].checked = source.checked;
   }
 }
 
 /* All session pause notication*/
 function togglePause(source) {
-  var checkboxes = document.getElementsByName('pause');
-  for(var i=0, n=checkboxes.length;i<n;i++) {
+  var check = document.getElementsByName('pause');
+  for(var i=0, n=check.length;i<n;i++) {
     /*alert(JSON.stringify(checkboxes));*/
-    checkboxes[i].checked = source.checked;
+    check[i].checked = source.checked;
   }
 }
 
 /* All session sleep notication*/
 function toggleSleep(source) {
-  var checkboxes = document.getElementsByName('sleep');
-  for(var i=0, n=checkboxes.length;i<n;i++) {
+  var check = document.getElementsByName('sleep');
+  for(var i=0, n=check.length;i<n;i++) {
     /*alert(JSON.stringify(checkboxes));*/
-    checkboxes[i].checked = source.checked;
+    check[i].checked = source.checked;
   }
 }
+
+function checkAllTurnOff(){
+	var turnoffBox = document.getElementsByName('turnoff');
+	var turnoffAll = document.getElementById('turnOffAll');
+	
+	var checked = 0;
+	var unchecked = 0;
+	
+	for(var i=0, n=turnoffBox.length;i<n;i++){
+		if(turnoffBox[i].checked){
+			checked++;
+		}	
+		else{ 
+			unchecked++;
+		}
+	}
+	
+	if(turnoffBox.length == checked){
+		turnoffAll.checked = true;
+	}else{
+		turnoffAll.checked = false;
+	}
+}
+
+
+/*check number of pause*/
+function checkAllPause(){
+	var pauseBox = document.getElementsByName('pause');
+	var pauseAll = document.getElementById('pauseAll');
+	
+	var checked = 0;
+	var unchecked = 0;
+	
+	for(var i=0, n=pauseBox.length;i<n;i++){
+		if(pauseBox[i].checked){
+			checked++;
+		}	
+		else{ 
+			unchecked++;
+		}
+	}
+	
+	if(pauseBox.length == checked){
+		pauseAll.checked = true;
+	}else{
+		pauseAll.checked = false;
+	}
+}
+
+/* check number of sleep*/
+function checkAllSleep(){
+	var sleepBox = document.getElementsByName('sleep');
+	var sleepAll = document.getElementById('sleepAll');
+	
+	var checked = 0;
+	var unchecked = 0;
+	
+	for(var i=0, n=sleepBox.length;i<n;i++){
+		if(sleepBox[i].checked){
+			checked++;
+		}	
+		else{ 
+			unchecked++;
+		}
+	}
+	
+	if(sleepBox.length == checked){
+		sleepAll.checked = true;
+	}else{
+		sleepAll.checked = false;
+	}
+}
+
 
