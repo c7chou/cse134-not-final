644dde2d1552f656b025ddc9c0ba55c23a915bb1
couchpotato4life <c1cortez@ucsd.edu>
2015-11-10 19:48:03 -0800

    Updated login page
    Created modal for sign up and forgot password, as well as check password
    matching and implemented password and email input limits

--- js/login.js
+++ js/login.js
@@ -2,3 +2,50 @@ function onClickSignUp() {
   var signUpText = document.getElementById("signInMessage");
   signUpText.style.display = "block";
 }
+
+function onClickForgot() {
+  var resendText = document.getElementById("resendMessage");
+  resendText.style.display = "block";
+}
+
+function onWrongPassword(){
+  var checkPassword = document.getElementById("checkPassword")
+  checkPassword.style.display = "block";
+}
+
+function isPasswordMatch(){
+  var password = document.getElementById("newPassword").value;
+  var confirmPassword = document.getElementById("retypeNewPassword").value;
+  
+  if (password != confirmPassword){
+   onWrongPassword();
+  }
+  else{
+   signUpConfirm();
+  }
+  
+}
+
+function signUpConfirm(){
+  onClickSignUp();
+  signUpOverlay();
+}
+
+function forgotConfirm(){
+  onClickForgot();
+  forgotOverlay();
+}
+
+function signUpOverlay() {
+	var el = document.getElementById("signUpOverlay");
+  var la = document.getElementById("overlay");
+	la.style.visibility = (la.style.visibility == "visible") ? "hidden" : "visible";
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+
+function forgotOverlay(){
+  var el = document.getElementById("forgotOverlay");
+	var la = document.getElementById("overlay");
+	la.style.visibility = (la.style.visibility == "visible") ? "hidden" : "visible";
+  el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
