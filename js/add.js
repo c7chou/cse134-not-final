15364e7c24e6355124221dda6bf9f27bb54d7438
c7chou <c7chou@ucsd.edu>
2015-12-04 10:23:40 -0800

    updated add.html

--- js/add.js
+++ js/add.js
@@ -1,3 +1,178 @@
+  /***Global Variables***/
+  var myDataRef = new Firebase('https://jjb750uy9yj.firebaseio-demo.com/habits/');
+	var uploadImageFlag = 0;
+
+  function selectImage(name) {
+    //Clear all the other effects
+    document.getElementById('icon1').style.border = "none";
+    document.getElementById('icon2').style.border = "none";
+    document.getElementById('icon3').style.border = "none";
+		document.getElementById('icon4').style.border = "none";
+		uploadImageFlag = 0;
+    var image = document.getElementById(name);
+    image.style.border = "5px solid #42A5F5";
+
+    var srcArray = image.src.split('/');
+    var src = srcArray[srcArray.length - 1]
+
+    //save current selected icon
+    selectedIcon = src;
+  }
+
+    function uploadImage()
+    {
+        $('#file-upload').trigger('click');
+		uploadImageFlag = 1;
+		
+        //document.getElementById("icon4").src = e.target.result;
+        //alert("wokred");
+    }
+
+    
+  $('#save').click(function(event) {
+
+    //Get element information
+    var name = $('#title').val();
+    var text = $('#others').val();
+
+    /*
+     * Title Check
+     */
+    if (name == ''){
+      title_ex.style.display = 'block';
+      title_text.style.color = 'red';
+      return;   
+    }
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    var daysOfWeek = {};
+    var checkedValues = document.getElementsByName('dayOfTheWeek');
+    var times = 0;
+    for(var i=0, n=checkedValues.length; i<n ; i++)
+    {
+      if(checkedValues[i].checked)
+      {
+        daysOfWeek[days[i]] = 'yes';
+        times++;
+      }
+      else
+      {
+        daysOfWeek[days[i]] = 'no';
+      }
+    }
+
+    /*
+     * Week Check
+     */
+
+    if (times == 0){
+        weekly_ex.style.display = 'block';
+        weekly_freq.style.color = 'red';
+        return;
+    }
+    var frequencyPerDay = document.getElementById('others').value;
+
+    var habitNameInput = document.getElementById('title').value;
+    //var habitOtherInput = document.getElementById('others').value;      
+    //habitOtherInput = Number(habitOtherInput);
+	  
+	if(uploadImageFlag)
+		{
+			selectedIcon = document.getElementById('icon4').src;
+		}
+	  else
+	  {
+		  selectedIcon = '../img/' + selectedIcon;
+	  }
+      
+    /* notification variable */ 
+
+    var notiHour = document.getElementById("notificationHours").value;
+    var notiMin = document.getElementById("notificationMins").value;
+
+
+
+      
+    /*************************/
+
+    /*
+     * Notification check
+     */
+    if (notiHour == ''){
+        hour_min_ex.style.display = 'block';
+        hour_min_ex.style.color = 'red';
+        hour_val.style.color = 'red';
+        return;
+    }
+        
+    if (notiMin == ''){
+        hour_min_ex.style.display = 'block';
+        hour_min_ex.style.color = 'red';
+        min_val.style.color = 'red';
+        return; 
+    }
+        
+      
+
+    /*
+     * Daily Check
+     */
+
+    if ((frequencyPerDay == undefined)||(isNaN(frequencyPerDay))||(frequencyPerDay<0)){
+      daily_ex.style.display = 'block';
+      daily_freq.style.color = 'red';
+      return;
+    }
+ 
+    var currentTime = Date.now();
+    if (!Date.now) {
+      Date.now = function() { return new Date().getTime(); }
+    }
+
+    if(/[^a-zA-Z0-9_\-\ /]/.test( frequencyPerDay ))
+    {
+      alert('Input must be alphanumeric. Please remove non-alphanumeric symbols to continue.');
+    }
+    else if(/[^a-zA-Z0-9\-\ /]/.test( habitNameInput ))
+    {
+      alert('Input must be alphanumeric. Please remove non-alphanumeric symbols to continue.');
+    }
+    else
+    {
+      //make firebase object and push it to DB
+      myDataRef.push({
+        habitData: {
+          name: name, 
+          text: text, 
+          'iconId': selectedIcon,
+          daysOfWeek: daysOfWeek,
+          frequencyPerDay: frequencyPerDay,
+          'settings': {
+            'turnoff': '0', 
+            'sleep':'0',
+            'pause':'0'
+          },
+          'habitProgress':{	
+            'counter': '0', 
+            'maxRecord':'1'
+          }, 
+          'timestamp': currentTime,
+          'notiTime':{
+            notiHour: notiHour,
+            notiMins: notiMin
+          }
+        }
+      });
+
+      document.location='list.html';
+    } 
+  }); 
+
+  $(document).ready(function() {
+      //event.preventDefault(); //prevent submit: for testing purposes
+      //alert("End.");
+  });
+
 
 
 /***********************************************************************************************
@@ -19,202 +194,6 @@ function isNumberInMin(input){
 
 
 
-/***********************************************************************************************
-*
-*  Notification
-*
-***********************************************************************************************
-
-// Once the service worker is registered set the initial state  
-function initialiseState() {  
-	// Are Notifications supported in the service worker?  
-	if (!('showNotification' in ServiceWorkerRegistration.prototype)) {  
-		console.warn('Notifications aren\'t supported.');  
-		return;  
-	}
-
-	// Check the current Notification permission.  
-	// If its denied, it's a permanent block until the  
-	// user changes the permission  
-	if (Notification.permission === 'denied') {  
-		console.warn('The user has blocked notifications.');  
-		return;  
-	}
-
-	// Check if push messaging is supported  
-	if (!('PushManager' in window)) {  
-		console.warn('Push messaging isn\'t supported.');  
-		return;  
-	}
-	
-	if('serviceWorker' in navigator){
-	// We need the service worker registration to check for a subscription  
-	navigator.serviceWorker.register('../js/add.js').then(function(serviceWorkerRegistration) {  
-		// Do we already have a push message subscription?  
-
-		serviceWorkerRegistration.pushManager.getSubscription()  
-			.then(function(subscription) {  
-				// Enable any UI which subscribes / unsubscribes from  
-				// push messages.
-				var pushButton = document.querySelector('.notif');  
-				pushButton.disabled = false;
-
-				if (!subscription) {  
-					// We aren't subscribed to push, so set UI  
-					// to allow the user to enable push  
-					return;  
-				}
-
-				// Keep your server in sync with the latest subscriptionId
-				sendSubscriptionToServer(subscription);
-
-				// Set your UI to show they have subscribed for  
-				// push messages  
-				pushButton.textContent = 'Disable Push Messages';  
-				isPushEnabled = true;  
-			})  
-			.catch(function(err) {  
-				console.warn('Error during getSubscription()', err);  
-			});  
-		});	  
-	}
-}
-
-/***********************************************************************************************
-*
-*  Notification
-*
-***********************************************************************************************
-
-function sub(){
-	var pushButton = document.querySelector('.notif');
-	if(pushButton.checked == false){
-		//unsubscribe();
-	}
-	else{
-		subscribe();
-	}
-}
-
-
-
-function subscribe() {  
-	// Disable the button so it can't be changed while  
-	// we process the permission request  
-	var pushButton = document.querySelector('.notif');  
-	var notificationBar = document.querySelector('#notificationIntervalBar');
-	//if(pushButton.disabled == false){
-		pushButton.disabled = true;
-	//}
-
-	navigator.serviceWorker.register('../js/add.js').then(function(serviceWorkerRegistration) {  
-		serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly:true})  
-			.then(function(subscription) {  
-				// The subscription was successful  
-				isPushEnabled = true;  
-				pushButton.textContent = 'Disable Push Messages';  
-				pushButton.disabled = false;
-
-				// TODO: Send the subscription.endpoint to your server  
-				// and save it to send a push message at a later date
-				return sendSubscriptionToServer(subscription);  
-			})  
-			.catch(function(e) {  
-				if (Notification.permission === 'denied') {  
-					// The user denied the notification permission which  
-					// means we failed to subscribe and the user will need  
-					// to manually change the notification permission to  
-					// subscribe to push messages  
-					console.warn('Permission for Notifications was denied');  
-					pushButton.disabled = true;  
-					pushButton.checked = false;
-				} else {  
-					// A problem occurred with the subscription; common reasons  
-					// include network errors, and lacking gcm_sender_id and/or  
-					// gcm_user_visible_only in the manifest.  
-					console.error('Unable to subscribe to push.', e);  
-					pushButton.disabled = false;  
-					pushButton.textContent = 'Enable Push Messages';  
-				}  
-			});  
-	});  
-}
-
-
-
-
-function unsubscribe() {  
-  var pushButton = document.querySelector('.notif');  
-  pushButton.disabled = true;
-
-  navigator.serviceWorker.register('../js/add.js').then(function(serviceWorkerRegistration) {  
-    // To unsubscribe from push messaging, you need get the  
-    // subscription object, which you can call unsubscribe() on.  
-    serviceWorkerRegistration.pushManager.getSubscription().then(  
-      function(pushSubscription) {  
-        // Check we have a subscription to unsubscribe  
-        if (!pushSubscription) {  
-          // No subscription object, so set the state  
-          // to allow the user to subscribe to push  
-          isPushEnabled = false;  
-          pushButton.disabled = false;  
-          pushButton.textContent = 'Enable Push Messages';  
-          return;  
-        }  
-
-        var subscriptionId = pushSubscription.subscriptionId;  
-        // make request from your server side 
-        // the subscriptionId from your data store so you
-        // don't attempt to send them push messages anymore
-
-        // We have a subscription, so call unsubscribe on it  
-        pushSubscription.unsubscribe().then(function(successful) {  
-          pushButton.disabled = false;  
-          pushButton.textContent = 'Enable Push Messages';  
-          isPushEnabled = false;  
-        }).catch(function(e) {  
-          // We failed to unsubscribe, this can lead to  
-          // an unusual state, so may be best to remove
-          // the users data from your data store and
-          // inform the user that you have done so
-
-          console.log('Unsubscription error: ', e);  
-          pushButton.disabled = false;
-          pushButton.textContent = 'Enable Push Messages';
-        });  
-      }).catch(function(e) {  
-        console.error('Error thrown while unsubscribing from push messaging.', e);  
-      });  
-  });  
-}
-
-
-
-
-self.addEventListener('push', function(event) {  
-  console.log('Received a push message', event);
-	
-	var titleName = document.getElementById('#title').value();
-		
-  var title = 'Yay a message.';  
-  var body = 'We have received a push message.';  
-  var icon = '/images/icon-192x192.png';  
-  var tag = 'simple-push-demo-notification-tag';
-
-  event.waitUntil(  
-    self.registration.showNotification(title, {  
-      body: body,  
-      icon: icon,  
-      tag: tag  
-    })  
-  );  
-});
-
-
-
-
-
-
 
 
 
@@ -228,32 +207,7 @@ self.addEventListener('push', function(event) {
 
 var isPushEnabled = false;
 
-/*
-window.addEventListener('load', function() {  
-var pushButton = document.querySelector('.js-push-button');  
-pushButton.addEventListener('click', function() {  
-	if (isPushEnabled) {  
-		unsubscribe();  
-	} else {  
-		subscribe();  
-	}  
-});*/
 
-// Check that service workers are supported, if so, progressively  
-// enhance and add push messaging support, otherwise continue without it.  
-/*if ('serviceWorker' in navigator) {  
-	navigator.serviceWorker.register('/service-worker.js')  
-	.then(initialiseState);  
-} else {  
-	console.warn('Service workers aren\'t supported in this browser.');  
-}  
-});*/
-
-
-/*
-const HR = 3600000;
-const MIN = 60000;
-*/
 
 function notifyMe() {
 	//check if notification is supported by broswer
@@ -297,26 +251,5 @@ function createNotification(strTitle, sIcon){
 	}
 	setTimeout(n.close.bind(n), 1000);
 }
-/*
-function setAutomatedNotification(hours, minutes){
-
-	var interval = hours*HR + minutes*MIN;
-	
-	var timestamp = new Date().getTime();
-	interval = days*DAY + hours*HR + minutes*MIN;
-	timestamp+= interval; 
-	timestamp = new Date(timestamp);
-
-	var timenow = new Date().getNow();
-	if( timenow >= timestamp){
-			createNotification()	
-	}
-	var displayNotification = setInterval(createNotification, interval);
-
-}
-function stopAutomatedNotification(habitCleared){
-	clearInterval(habitCleared);
-}
-*/
 
 				
